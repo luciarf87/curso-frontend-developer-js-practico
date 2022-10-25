d303488f439d5bacac70659229b088fcdbc70b17
Lucia Rodriguez Fauve <luciarf87@gmail.com>
2022-10-25 17:12:56 -0300

    Fusión del mobile desktop al archivo principal con JS  y arreglo de media querys en css

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const navMenuLogo = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+navMenuLogo.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive');
+}
+function toggleMobileMenu () {
+    mobileMenu.classList.toggle('inactive');
 }
