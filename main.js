98c1f4fe9752b7f006c95fe7ad778dc995746f36
Lucia Rodriguez Fauve <luciarf87@gmail.com>
2022-10-26 15:40:14 -0300

    Agregué carrito de compras al menu desktop y mobile

--- main.js
+++ main.js
@@ -2,13 +2,35 @@ const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const navMenuLogo = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const cartIcon = document.querySelector('.navbar-shopping-cart');
+const cartMenu = document.querySelector('.product-detail')
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 navMenuLogo.addEventListener('click', toggleMobileMenu);
+cartIcon.addEventListener('click', toggleCartMenu);
 
 function toggleDesktopMenu () {
+    const iscartMenuClose = cartMenu.classList.contains('inactive');
+    if(!iscartMenuClose) {
+        cartMenu.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 function toggleMobileMenu () {
+    const iscartMenuClose = cartMenu.classList.contains('inactive');
+    if(!iscartMenuClose) {
+        cartMenu.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+function toggleCartMenu () {
+    const ismobileMenuClose = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClose = desktopMenu.classList.contains('inactive'); 
+   if(!ismobileMenuClose) {
+            mobileMenu.classList.add('inactive');
+        }
+    else if(!isDesktopMenuClose) {
+        desktopMenu.classList.add('inactive');
+        }
+    cartMenu.classList.toggle('inactive');
 }
