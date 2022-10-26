a029c198d3bd0aa64fb5bd5ebd34b431a30cfe6e
Lucia Rodriguez Fauve <luciarf87@gmail.com>
2022-10-26 20:29:23 -0300

    Inserté la lista de productos al main

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const navMenuLogo = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const cartIcon = document.querySelector('.navbar-shopping-cart');
 const cartMenu = document.querySelector('.product-detail')
+const cardsContainer = document.querySelector('.cards-container');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 navMenuLogo.addEventListener('click', toggleMobileMenu);
@@ -34,3 +35,60 @@ function toggleCartMenu () {
         }
     cartMenu.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/400678/pexels-photo-400678.jpeg?cs=srgb&dl=pexels-karol-d-400678.jpg&fm=jpg'
+});
+productList.push({
+    name: 'Notebook',
+    price: 500,
+    image: 'https://images.pexels.com/photos/1229861/pexels-photo-1229861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+function renderProducts(arr){
+    for(product of arr) {
+       const productCard = document.createElement('div');
+       productCard.classList.add('product-card');
+    
+       const productImg = document.createElement('img');
+       productImg.setAttribute('src', product.image);
+    
+       const productInfo = document.createElement('div');
+       productInfo.classList.add('product-info');
+    
+       const productInfoDiv = document.createElement('div');
+    
+       const productPrice = document.createElement('p');
+       productPrice.innerText = '$' + product.price;
+      
+       const productName = document.createElement('p');
+       productName.innerText = product.name;
+    
+       productInfoDiv.appendChild(productPrice);
+       productInfoDiv.appendChild(productName);
+    
+       const productInfoFigure = document.createElement('figure');
+       const productImgCart = document.createElement('img');
+       productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+       productInfoFigure.appendChild(productImgCart);
+    
+       productInfo.appendChild(productInfoDiv);
+       productInfo.appendChild(productInfoFigure);
+    
+       productCard.appendChild(productImg);
+       productCard.appendChild(productInfo);
+    
+       cardsContainer.appendChild(productCard);
+    };
+
+}
+renderProducts(productList);
+ 
